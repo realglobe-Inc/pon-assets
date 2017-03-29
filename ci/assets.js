bd15a4aec5d8f268e54f1f735fa8e5ebe448ebc1
Taka Okunishi <okunishinish@gmail.com>
2017-03-29 12:42:48 +0900

    pon用のbannerを用意した

---
+++ ci/assets.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+/**
+ * Generate asset files
+ */
+'use strict'
+
+process.chdir(`${__dirname}/..`)
+
+const apeTasking = require('ape-tasking')
+const co = require('co')
+const { banner, favicon } = require('../lib')
+
+const assetDir = `${__dirname}/../assets`
+
+apeTasking.runTasks('asset', [
+  () => co(function * () {
+    let banners = {
+      'images/pon-banner.png': [ 'pon', {} ],
+      'images/pon-logger-banner.png': [ 'core', { name: 'Pon-Logger' } ],
+      'images/pon-cli-banner.png': [ 'core', { name: 'Pon-CLI' } ],
+      'images/ponfile-banner.png': [ 'core', { name: 'Ponfile' } ],
+      'images/pon-timer-banner.png': [ 'core', { name: 'Pon-Timer' } ],
+      'images/pon-context-banner.png': [ 'core', { name: 'Pon-Context' } ],
+    }
+    for (let filename of Object.keys(banners)) {
+      let [ type, config ] = banners[ filename ]
+      yield banner(type, `${assetDir}/${filename}`, config)
+    }
+  }),
+  () => co(function * () {
+    let favicons = {
+      'images/pon-favicon.png': [ 'pon', {} ]
+    }
+    for (let filename of Object.keys(favicons)) {
+      let [ type, config ] = favicons[ filename ]
+      yield favicon(type, `${assetDir}/${filename}`, config)
+    }
+  })
+], true)
---
+++ test/install_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for install.
+ * Runs with mocha.
+ */
+'use strict'
+
+const install = require('../lib/install.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('install', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Install', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
